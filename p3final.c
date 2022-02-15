f60a369a690551ab7391e06fd7c66b2e7cdbc0b2
gayatri120613 <94883116+Gayatri120613@users.noreply.github.com>
2022-02-15 04:00:25 +0000

    done

--- p3final.c
+++ p3final.c
@@ -1,45 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int input()
 {
-int a;
-printf("Enter a number\n");
-scanf("%d", &a);
-return a;
+  int n;
+  printf("enter a number\n");
+  scanf("%d",&n);
+  return n;
 }
-void input_array(int n, int a[n])
+int calculate (int n)
 {
-  for(int i=0;i<n;i++)
+  int i, sum=0;
+  for (i=1;i<=n;++i)
   {
-    printf("Enter the element no %d of the array\n",i);
-    scanf("%d",&a[i]);
-  }
-}
-
-int array_sum(int n, int a[n])
-{
-  int sum=0;
-  for(int i=0;i<n;i++)
-  {
-    sum += a[i];
+    sum +=i;
   }
   return sum;
 }
-int output(int n, int a[n], int sum)
+int output(int n, int sum)
 {
-  int i;
-  printf("The sum of");
-  for(i=0;i<n-1;i++)
-  {
-    printf("%d + ",a[i]);
-  }
-  printf("%d = %d\n", a[i], sum);
+  printf("sum=%d", sum);
+  return 0;
 }
 int main()
 {
-  int n,sum;
-  n=input();
-  int a[n];
-  input_array(n,a);
-  sum=array_sum(n,a);
-  output(n,a,sum);
+  int a=input();
+  int b=calculate(a);
+  output(a,b);
+  return 0;
 }
